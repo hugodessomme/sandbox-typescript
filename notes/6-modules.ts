435bdb53d6c4e4ed2e18e93880c76990382e8e95
hugodessomme <hdessomme@gmail.com>
2019-05-05 14:54:02 +0200

    Add namespaces and modules

---
+++ notes/6-modules.ts
@@ -0,0 +1,5 @@
+import { PI, calculateCircumference } from "./circle";
+import calcRect from "./rectangle";
+console.log(PI);
+console.log(calculateCircumference(10));
+console.log(calcRect(20, 50));
